8a65f7acb07ce9e4298fa6392af1ed2e1dabf76f
Francisco Javier Barba <barbatrejofco@gmail.com>
2023-02-23 12:49:29 +0100

    Task 033: Done

---
+++ src/main/java/acme/entities/offer/Offer.java
@@ -0,0 +1,55 @@
+
+package acme.entities.offer;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.PositiveOrZero;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Offer extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	protected Date				moment;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			heading;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			summary;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				availabilityStartDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				availabilityEndDate;
+
+	@PositiveOrZero
+	protected Boolean			price;
+
+	@URL
+	protected String			link;
+
+}
