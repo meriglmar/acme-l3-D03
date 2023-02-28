355640f90a9c5b8560a507d04666d684949acc9b
Dr.Kenobi27 <josgarber6@alum.us.es>
2023-02-28 13:12:31 +0100

    Task 058: Done

---
+++ src/main/java/acme/entities/sessions/Session.java
@@ -0,0 +1,44 @@
+
+package acme.entities.sessions;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Session extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			abstract_;
+
+	protected Boolean			theorySession;
+
+	protected LocalDateTime		initTimePeriod;
+
+	protected LocalDateTime		finishTimePeriod;
+
+	@URL
+	protected String			link;
+
+}
