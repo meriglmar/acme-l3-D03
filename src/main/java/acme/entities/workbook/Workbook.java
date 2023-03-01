87518f9f7a4e19176f3cb77a68555ed09dce9d63
abernalmar <abernalmar@gmail.com>
2023-03-01 18:47:31 +0100

    Task 050: In review

---
+++ src/main/java/acme/entities/workbook/Workbook.java
@@ -0,0 +1,45 @@
+
+package acme.entities.workbook;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Workbook extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(max = 76)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 101)
+	protected String			abstractt;
+
+	@NotBlank
+	protected ActivityType		activityType;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				timePeriod;
+
+	@URL
+	protected String			link;
+}
