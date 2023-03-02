44e791e142d201d508d01d1129a778d7fb3c21c8
paomarsan <paomarsan@alum.us.es>
2023-03-02 14:14:50 +0100

    Task 070: In progress

---
+++ src/main/java/acme/entities/audits/Audit.java
@@ -0,0 +1,55 @@
+
+package acme.entities.audits;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.PositiveOrZero;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Audit extends AbstractEntity {
+
+	//Una auditoría es un documento con registros de auditoría sobre un curso publicado. 
+	//El sistema debe almacenar los siguientes datos sobre ellos: 
+	//un código (patrón “[A-Z]{1,3}[0-9][0-9]{3}”, no en blanco, único)
+	//una conclusión (no en blanco, menos de 101 caracteres)
+	//algunos puntos fuertes (no en blanco, menos de 101 caracteres)
+	//algunos puntos débiles (no en blanco, menos de 101 caracteres)
+	//una nota (computada como la moda de las notas en los registros de auditoría correspondientes; empates debe romperse arbitrariamente si es necesario).
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			conclusion;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			strongPoints;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			weakPoints;
+
+	@PositiveOrZero
+	protected int				mark; //una nota (computada como la moda de las notas en los registros de auditoría correspondientes; empates debe romperse arbitrariamente si es necesario).
+
+}
