ff1b146b60f75f349e5ac8ed151c0d3d9fb3f32d
meriglmar <meriglmar@alum.us.es>
2023-03-04 14:48:57 +0100

    Task 063: In review

--- src/main/java/acme/entities/practicums/Practicum.java
+++ src/main/java/acme/entities/practicums/Practicum.java
@@ -7,7 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.validation.Valid;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -36,47 +36,46 @@ public class Practicum extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
 
-	protected static final long			serialVersionUID	= 1L;
+	protected static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
 
 	@NotBlank
 	@Column(unique = true)
 	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
-	protected String					code;
+	protected String			code;
 
 	@NotBlank
 	@Length(max = 75)
-	protected String					title;
+	protected String			title;
 
 	@NotBlank
 	@Length(max = 100)
-	protected String					summary;
+	protected String			summary;
 
 	@NotBlank
 	@Length(max = 100)
-	protected String					goals;
-
-	//	@PositiveOrZero
-	//	protected int						estimatedTotalTime;
+	protected String			goals;
 
 	// Derived attributes -----------------------------------------------------
 
-	//	@Transient
-	//	protected double estimatedTotalTime() {
-	//		final double totalTime = 0;
-	//		for (final PracticumSession pSession : this.practicumSessions)
-	//			totalTime += this.practicumSessions.getDuration();
-	//		return totalTime * 1.1;
-	//
-	//	}
+
+	@Transient
+	protected double estimatedTotalTime() {
+		double totalTime = 0;
+		for (final PracticumSession pSession : this.practicumSessions)
+			totalTime += pSession.getDuration();
+		return totalTime * 1.1;
+
+	}
+
 
 	// Relationships ----------------------------------------------------------
 	@NotNull
 	@OneToMany(mappedBy = "practicum")
 	protected List<PracticumSession>	practicumSessions;
 
-	@Valid
+	//@Valid
 	@NotNull
 	@ManyToOne(optional = false)
 	protected Company					company;
