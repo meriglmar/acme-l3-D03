c268cf25d9dbf82c61f658b5c603009cb06ac265
paomarsan <paomarsan@alum.us.es>
2023-03-14 12:15:49 +0100

    Task 070: Relation with auditor

--- src/main/java/acme/entities/audits/Audit.java
+++ src/main/java/acme/entities/audits/Audit.java
@@ -10,14 +10,17 @@ import java.util.stream.Collectors;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 
 import acme.entities.auditingRecords.TypeMark;
 import acme.framework.data.AbstractEntity;
+import acme.roles.Auditor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -69,6 +72,11 @@ public class Audit extends AbstractEntity {
 
 		final Iterator<TypeMark> iterator = sortedMap.keySet().iterator();
 		return iterator.next();
-
 	}
+
+
+	// Relationships ----------------------------------------------------------
+	@NotNull
+	@ManyToOne
+	protected Auditor auditor;
 }
