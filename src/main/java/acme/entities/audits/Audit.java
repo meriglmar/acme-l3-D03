b995c4ba8c47f75f71181d86ebaab0d4e5479f4c
paomarsan <paomarsan@alum.us.es>
2023-03-14 12:19:24 +0100

    Task 070: relation with auditingRecord

--- src/main/java/acme/entities/audits/Audit.java
+++ src/main/java/acme/entities/audits/Audit.java
@@ -11,13 +11,16 @@ import java.util.stream.Collectors;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Transient;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 
+import acme.entities.auditingRecords.AuditingRecord;
 import acme.entities.auditingRecords.TypeMark;
 import acme.framework.data.AbstractEntity;
 import acme.roles.Auditor;
@@ -76,7 +79,12 @@ public class Audit extends AbstractEntity {
 
 
 	// Relationships ----------------------------------------------------------
+	@Valid
 	@NotNull
-	@ManyToOne
-	protected Auditor auditor;
+	@ManyToOne(optional = false)
+	protected Auditor				auditor;
+
+	@NotNull
+	@OneToMany(mappedBy = "audit")
+	protected List<AuditingRecord>	auditingRecord;
 }
