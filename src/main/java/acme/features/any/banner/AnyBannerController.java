35e6414f8277eabf486aafa43b98ffde2603c961
abernalmar <abernalmar@gmail.com>
2023-03-28 19:05:51 +0200

    Task 084: In progress

--- src/main/java/acme/features/any/banner/AnyBannerController.java
+++ src/main/java/acme/features/any/banner/AnyBannerController.java
@@ -1,9 +1,24 @@
 
 package acme.features.any.banner;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-@Controller
-public class AnyBannerController {
+import acme.entities.banners.Banner;
+import acme.framework.components.accounts.Any;
+import acme.framework.controllers.AbstractController;
 
+@Controller
+public class AnyBannerController extends AbstractController<Any, Banner> {
+
+	@Autowired
+	private AnyBannerShowService bannerService;
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("show", this.bannerService);
+	}
 }
--- src/main/java/acme/features/any/banner/AnyBannerShowService.java
+++ src/main/java/acme/features/any/banner/AnyBannerShowService.java
@@ -1,9 +1,55 @@
 
 package acme.features.any.banner;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import acme.entities.banners.Banner;
+import acme.framework.components.accounts.Any;
+import acme.framework.helpers.MomentHelper;
+import acme.framework.services.AbstractService;
+
 @Service
-public class AnyBannerShowService {
+public class AnyBannerShowService extends AbstractService<Any, Banner> {
+
+	@Autowired
+	protected AnyBannerRepository bannerRepository;
+
+
+	@Override
+	public void check() {
+		boolean status;
+
+		status = super.getRequest().hasData("id", int.class);
+
+		super.getResponse().setChecked(status);
+	}
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		List<Banner> objects;
+		Date currentMoment;
+
+		currentMoment = MomentHelper.getCurrentMoment();
+		objects = this.bannerRepository.findActiveBanners(currentMoment);
+		super.getBuffer().setData(objects);
+	}
+
+	public Banner getRandomActiveBanner() {
+		final List<Banner> activeBanners = this.bannerRepository.findActiveBanners(new Date());
+		if (activeBanners.isEmpty())
+			return null;
+		final int randomIndex = new Random().nextInt(activeBanners.size());
+		return activeBanners.get(randomIndex);
+	}
 
 }
