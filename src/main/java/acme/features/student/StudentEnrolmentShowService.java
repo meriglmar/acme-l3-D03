c48baaaea50992053f03ceb4b3a5cff67c460ec8
abernalmar <abernalmar@gmail.com>
2023-04-04 12:55:30 +0200

    Task 102: In progress

---
+++ src/main/java/acme/features/student/StudentEnrolmentShowService.java
@@ -0,0 +1,67 @@
+
+package acme.features.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.enrolments.Enrolment;
+import acme.framework.components.models.Tuple;
+import acme.framework.services.AbstractService;
+import acme.roles.Student;
+
+@Service
+public class StudentEnrolmentShowService extends AbstractService<Student, Enrolment> {
+	// Internal state --------------------------------------------------
+
+	@Autowired
+	protected StudentEnrolmentRepository repository;
+
+	// AbstractShowService<Inventor, Item> interface --------------------
+
+
+	@Override
+	public void check() {
+		boolean status;
+
+		status = super.getRequest().hasData("id", int.class);
+
+		super.getResponse().setChecked(status);
+	}
+
+	@Override
+	public void authorise() {
+		boolean status;
+		int enrolmentId;
+		Enrolment enrolment;
+
+		enrolmentId = super.getRequest().getData("id", int.class);
+		enrolment = this.repository.findEnrolmentById(enrolmentId);
+		status = enrolment != null && super.getRequest().getPrincipal().hasRole(Student.class);
+
+		super.getResponse().setAuthorised(status);
+	}
+
+	@Override
+	public void load() {
+		Enrolment enrolment;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		enrolment = this.repository.findEnrolmentById(id);
+
+		super.getBuffer().setData(enrolment);
+	}
+
+	@Override
+	public void unbind(final Enrolment object) {
+		assert object != null;
+
+		Tuple tuple;
+
+		tuple = super.unbind(object, "code", "motivation", "goals", "workTime", "finalised");
+		tuple.put("finalisedMode", object.isFinalised());
+
+		super.getResponse().setData(tuple);
+	}
+
+}
