c6409ce99c3405c6ba1955d35acd0f2d231ba897
paomarsan <paomarsan@alum.us.es>
2023-04-11 14:20:44 +0200

    Task 126: In progress

---
+++ src/main/java/acme/features/auditor/audit/AuditorAuditCreateService.java
@@ -0,0 +1,66 @@
+
+package acme.features.auditor.audit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.audits.Audit;
+import acme.entities.course.Course;
+import acme.framework.services.AbstractService;
+import acme.roles.Auditor;
+
+@Service
+public class AuditorAuditCreateService extends AbstractService<Auditor, Audit> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected AuditorAuditRepository repo;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void check() {
+		super.getResponse().setChecked(true);
+	}
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Audit object;
+		object = new Audit();
+		final Auditor auditor = this.repo.findAuditorById(super.getRequest().getPrincipal().getActiveRoleId());
+		object.setAuditor(auditor);
+		super.getBuffer().setData(object);
+	}
+
+	@Override
+	public void bind(final Audit object) {
+		assert object != null;
+		int courseId;
+		Course course;
+		courseId = super.getRequest().getData("course", int.class);
+		course = this.repo.findCourseById(courseId);
+		super.bind(object, "code", "conclusion", "strongPoints", "weakPoints");
+	}
+
+	@Override
+	public void validate(final Audit object) {
+		assert object != null;
+		if (!super.getBuffer().getErrors().hasErrors("code"))
+			super.state(this.repo.findAuditByCode(object.getCode()) == null, "code", "auditor.audit.form.error.code");
+
+	}
+
+	@Override
+	public void perform(final Audit object) {
+		assert object != null;
+		this.repo.save(object);
+	}
+
+}
