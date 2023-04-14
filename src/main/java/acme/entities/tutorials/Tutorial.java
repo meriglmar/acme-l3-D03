1310c4f0e810dcb97b2c27b92463b787b7af16d8
Dr.Kenobi27 <josgarber6@alum.us.es>
2023-04-14 22:48:46 +0200

    Task 107: fixed (@Valid en tutorial con Assistant)

--- src/main/java/acme/entities/tutorials/Tutorial.java
+++ src/main/java/acme/entities/tutorials/Tutorial.java
@@ -5,6 +5,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Transient;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -66,6 +67,7 @@ public class Tutorial extends AbstractEntity {
 	protected Course	course;
 
 	@NotNull
+	@Valid
 	@ManyToOne(optional = false)
 	protected Assistant	assistant;
 
