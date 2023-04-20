bcf9a2cc29290601e4796241ac0f9ae527c67618
meriglmar <meriglmar@alum.us.es>
2023-04-20 10:41:06 +0200

    Task 083: fixed

--- src/main/java/acme/features/authenticated/note/AuthenticatedNoteCreateService.java
+++ src/main/java/acme/features/authenticated/note/AuthenticatedNoteCreateService.java
@@ -84,7 +84,7 @@ public class AuthenticatedNoteCreateService extends AbstractService<Authenticate
 		//			super.state(this.auxiliarService.validateTextImput(object.getEmail()), "email", "authenticated.note.form.error.spam");
 		//
 		//		if (!super.getBuffer().getErrors().hasErrors("furtherInformationLink"))
-		//			super.state(this.auxiliarService.validatvalidaeTextImput(object.getFurtherInformationLink()), "furtherInformationLink", "authenticated.note.form.error.spam");
+		//			super.state(this.auxiliarService.validateTextImput(object.getFurtherInformationLink()), "furtherInformationLink", "authenticated.note.form.error.spam");
 	}
 
 	@Override
