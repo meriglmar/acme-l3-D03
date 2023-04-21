e4f5a3927125987addf1fbc7a63f1f280ec5cb3e
meriglmar <meriglmar@alum.us.es>
2023-04-21 20:51:41 +0200

    Task 118: practicums created

--- src/main/java/acme/features/company/practicumSession/CompanyPracticumSessionDeleteService.java
+++ src/main/java/acme/features/company/practicumSession/CompanyPracticumSessionDeleteService.java
@@ -110,7 +110,7 @@ public class CompanyPracticumSessionDeleteService extends AbstractService<Compan
 		choices = SelectChoices.from(practica, "code", object.getPracticum());
 		Tuple tuple;
 
-		tuple = super.unbind(object, "title", "abstract$", "startPeriod", "finishPeriod", "draftMode", "exceptional", "optionalLink");
+		tuple = super.unbind(object, "title", "abstract$", "startPeriod", "finishPeriod", "draftMode", "exceptional", "optionalLink", "estimatedTotalTime");
 		tuple.put("practicum", choices.getSelected().getKey());
 		tuple.put("practica", choices);
 
